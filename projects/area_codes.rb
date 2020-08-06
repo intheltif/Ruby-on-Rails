5f410fd9a46090106d60524f811d3a68975ba2da
Evert Ball <evert.ball@gmail.com>
2020-08-06 10:51:22 -0400

    Updated comments.

--- projects/area_codes.rb
+++ projects/area_codes.rb
@@ -1,3 +1,4 @@
+# The hash that contains the city to area code mapping
 dial_book = {
   "newyork" => "212",
   "newbrunswick" => "732",
@@ -13,28 +14,34 @@ dial_book = {
  
 # Get city names from the hash
 def get_city_names(somehash)
+  # return an array of the keys of the hash parameter
   return somehash.keys
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
+  # Check that the hash has a mapping to the specified key
   if somehash.include? key
+    # Return the string we want to show the user of the city to area code map
     return "The area code for #{key} is #{somehash[key]}"
   else
+    # Return the below statement if the hash does not contain the key
     return "That city does not exist."
   end
 end
  
 # Execution flow
 loop do
-# Write your program execution code here
+  # Provided code
   puts "Do you want to lookup an area code based on a city name?(Y/N)"
   answer = gets.chomp.downcase
   break if answer != "y"
-
+  
+  # Print out the keys to the screen
   city_names = get_city_names(dial_book)
   city_names.each { |city| puts city }
-
+  
+  # Have user choose a city and print out the result
   puts "Which city do you want the area code for?"
   selection = gets.chomp.downcase
   puts get_area_code(dial_book, selection)
