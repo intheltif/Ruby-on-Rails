4f498617336ce6e7cf76dfe84f1da41d0ebff969
Evert Ball <evert.ball@gmail.com>
2020-08-06 16:35:02 -0400

    Created an object to explore mixins, classes, and accessors.

--- projects/crud.rb
+++ projects/crud.rb
@@ -2,22 +2,22 @@
 module Crud
   require 'bcrypt'
 
-  def Crud.create_hash_digest(password)
+  def create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
-  def Crud.verify_hash_digest(password)
+  def verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
-  def Crud.create_secure_users(list_of_users)
+  def self.create_secure_users(list_of_users)
     list_of_users.each { |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     }
     list_of_users
   end
 
-  def Crud.authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username] == username && 
       verify_hash_digest(user_record[:password]) == password
