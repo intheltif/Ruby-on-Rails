52e9e2d5bad456e05d2b6d146f7d7086868bd0b2
Evert Ball <evert.ball@gmail.com>
2020-08-06 09:21:19 -0400

    Updated indentation to fit ruby style

--- projects/authenticator.rb
+++ projects/authenticator.rb
@@ -6,13 +6,14 @@ users = [
   {username: "heisenberg", password: "password5"},
 ]
 def auth_user(username, password, list_of_users)
-    list_of_users.each do |user_record|
-        if user_record[:username] == username && user_record[:password] == password
-            return user_record
-        end
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
     end
-    return "Credentials were not correct."
+  end
+  "Credentials were not correct."
 end
+
 welcome_msg = "Welcome to the authenticator"
 puts welcome_msg
 welcome_msg.length.times { print "-" }
@@ -22,19 +23,19 @@ puts "If password is correct, you will get back the user object."
 
 attempts = 1
 while attempts < 4
-    print "Username: "
-    username = gets.chomp
-    print "Password: "
-    password = gets.chomp
-    
-    auth = auth_user(username, password, users)
-    puts auth
-   
-   puts "Press n to quit or any other key to continue: "
-   input = gets.chomp.downcase
-   break if input == "n"
-
-   attempts += 1 
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  
+  auth = auth_user(username, password, users)
+  puts auth
+ 
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+ 
+  attempts += 1 
 end
 
 puts "You have exceeded the number of attempts" if attempts == 4
